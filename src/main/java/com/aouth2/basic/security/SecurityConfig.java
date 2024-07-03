7b802cf4b925df848ff6ea730f3e18ed9dc79118
sadakhat <sadakhat11.khan@gmail.com>
2024-07-03 07:55:40 +0530

    initial commit

--- src/main/java/com/aouth2/basic/security/SecurityConfig.java
+++ src/main/java/com/aouth2/basic/security/SecurityConfig.java
@@ -34,19 +34,6 @@ public class SecurityConfig {
 
 
     private ClientRegistration googleClientRegistration() {
-        return ClientRegistration.withRegistrationId("google")
-                .clientId("802356167789-pfm7slsqm25q575efjq4oe9flfe3ptit.apps.googleusercontent.com")
-                .clientSecret("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")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-                .scope("openid", "profile", "email", "address", "phone")
-                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
-                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-                .clientName("Google")
-                .build();
+        return null;
     }
 }
